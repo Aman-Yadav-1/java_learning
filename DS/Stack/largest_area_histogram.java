17e071d724f8bd1e6593c62b5c2b25637902e911
Aman-yadav-ay <am4n.yadav@gmail.com>
2022-10-24 18:12:57 +0530

    lah

--- DS/Stack/largest_area_histogram.java
+++ DS/Stack/largest_area_histogram.java
@@ -22,7 +22,7 @@ public class largest_area_histogram {
         rb[arr.length - 1] = arr.length;
 
         for(int i = arr.length - 2;i >= 0;i--){
-            while(st.size() > 0 && arr[i] < arr[st.peek()]){
+            while(st.size() > 0 && arr[i] <= arr[st.peek()]){
                 st.pop();
             }
             if(st.size() == 0){
@@ -41,7 +41,7 @@ public class largest_area_histogram {
         lb[0] = -1;
 
         for(int i = 1;i < arr.length;i++){
-            while(st.size() > 0 && arr[i] < arr[st.peek()]){
+            while(st.size() > 0 && arr[i] <= arr[st.peek()]){
                 st.pop();
             }
             if(st.size() == 0){

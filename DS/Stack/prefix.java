00f859f670c06f1f48cdb748327d44e5ba7b1c0f
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-04 23:43:18 +0530

    prefix

---
+++ DS/Stack/prefix.java
@@ -0,0 +1,20 @@
+package DS.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class prefix {
+    public static void main(String [] args)throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String exp = br.readLine();
+    
+    //code
+    Stack<Integer> vs = new Stack<>();
+    Stack<String> is = new Stack<>();
+    Stack<String> ps = new Stack<>();
+
+    for(int i = exp.length() - 1;i >= 0;i--){
+        
+    }
+}
+}

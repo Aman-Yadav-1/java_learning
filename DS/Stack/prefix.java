5ca0e05a1559ac36625919c9348afe7a2699a615
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-06 20:35:04 +0530

    prefix

--- DS/Stack/prefix.java
+++ DS/Stack/prefix.java
@@ -12,7 +12,6 @@ public class prefix {
     Stack<Integer> vs = new Stack<>();
     Stack<String> is = new Stack<>();
     Stack<String> ps = new Stack<>();
-
     for(int i = exp.length() - 1;i >= 0;i--){
         char ch = exp.charAt(i);
         

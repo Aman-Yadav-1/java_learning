851673ffc0cfce057204058426f6d04f315a13f3
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-07 23:39:22 +0530

    ngetr

--- DS/Stack/ngetr_alt.java
+++ DS/Stack/ngetr_alt.java
@@ -4,6 +4,7 @@ package DS.Stack;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
+
 public class ngetr_alt {
     public static void display(int [] a){
         StringBuilder sb = new StringBuilder();

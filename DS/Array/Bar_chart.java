11f7fec8a8663f5a3582de5216863f6a00da4ca0
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-11-11 23:12:07 +0530

    bar

--- DS/Array/Bar_chart.java
+++ DS/Array/Bar_chart.java
@@ -9,7 +9,6 @@ public class Bar_chart {
         int n = scan.nextInt();
         
         int [] arr = new int[n];
-        
         for(int i = 0;i < arr.length;i++){
             arr[i] = scan.nextInt();
         }

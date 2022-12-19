5324f6d8e498d6ec7011cdbc6c502ba5c190810b
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-12-19 20:16:00 +0530

    bar

--- DS/Array/Bar_chart.java
+++ DS/Array/Bar_chart.java
@@ -14,7 +14,6 @@ public class Bar_chart {
         }
         
         int max = arr[0];
-        
         for(int i = 1;i < arr.length;i++){
             if(arr[i] > max){
                 max = arr[i];

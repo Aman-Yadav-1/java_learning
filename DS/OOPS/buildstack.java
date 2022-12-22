6da4128dc07e52ddc7263a9d41b58447de70c948
Aman-yadav-ay <amxn.yadav@gmail.com>
2022-12-22 15:16:42 +0530

    build-stack

--- DS/OOPS/buildstack.java
+++ DS/OOPS/buildstack.java
@@ -2,5 +2,50 @@
 package DS.OOPS;
 
 public class buildstack {
-    
+    public static class customStack{
+        int [] data;
+        int tos;
+        public customStack(int cap){
+            data = new int[cap];
+            tos = -1;
+        }
+        int size(){
+            return tos+1;
+        }
+        void display(){
+            for(int i = tos;i >= 0;i--){
+                System.out.print(data[i] + " ");
+            }
+            System.out.println();
+        }
+        void push(int val){
+            if(tos == data.length - 1){
+                System.out.println("Stack Overflow");
+            }
+            else{
+                tos++;
+                data[tos] = val;
+            }
+        }
+        int pop(){
+            if(tos == -1){
+                System.out.println("Stack Underflow");
+                return -1;
+            }
+            else{
+                int val = data[tos];
+                tos--;
+                return val;
+            }
+        }
+        int top(){
+            if(tos == -1){
+                System.out.println("Stack Underflow");
+                return -1;
+            }
+            else{
+                return data[tos];
+            }
+        }
+    }
 }

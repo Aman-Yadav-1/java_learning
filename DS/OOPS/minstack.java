72448fddd723fe1b79235c13312cefcf00278d3a
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-01-12 23:42:07 +0530

    m

--- DS/OOPS/minstack.java
+++ DS/OOPS/minstack.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+
 public class minstack {
     public static class MinStack{
         Stack <Integer> allData;

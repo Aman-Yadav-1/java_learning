f2d6cda3b00868a7bcbaa2a1ab774deb5e3ce8ef
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-01-22 00:49:48 +0530

    thk gya hu exam de de ke

--- DS/OOPS/minstack.java
+++ DS/OOPS/minstack.java
@@ -3,7 +3,6 @@ package DS.OOPS;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
-
 public class minstack {
     public static class MinStack{
         Stack <Integer> allData;

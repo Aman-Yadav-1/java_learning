ba09f14150a6f0e9daeb5b7da82e026cfd807633
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-03 16:49:00 +0530

    jagged_array

---
+++ gfg_fork/Arrays/jagged_array.java
@@ -0,0 +1,28 @@
+/*Jagged array referes to a array that consists of different rows that have a different columns in them
+ * Like first row has 2 columns
+ * 2nd row has 3 columns
+*/
+package gfg_fork.Arrays;
+
+public class jagged_array {
+    public static void main(String[] args){
+        int arr[][] = new int[2][];
+        
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+        int count=0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                arr[i][j]=count++;
+            }
+        }
+        System.out.println("Content Of Jagged Array");
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}

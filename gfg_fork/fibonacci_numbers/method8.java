52f83915bd279b6a06eeca633f7f99cb2271690b
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 01:47:21 +0530

    m8

---
+++ gfg_fork/fibonacci_numbers/method8.java
@@ -0,0 +1,41 @@
+package gfg_fork.fibonacci_numbers;
+
+import java.util.*;
+
+public class method8 {
+    // Initialize array of dp
+static int[] dp = new int[10];
+
+static int fib(int n)
+{
+    if (n <= 1)
+        return n;
+        
+    // Temporary variables to store
+    // values of fib(n-1) & fib(n-2)
+    int first, second;
+    
+    if (dp[n - 1] != -1)
+        first = dp[n - 1];
+    else
+        first = fib(n - 1);
+
+    if (dp[n - 2] != -1)
+        second = dp[n - 2];
+    else
+        second = fib(n - 2);
+
+    // Memoization
+    return dp[n] = first + second;
+}
+
+// Driver Code
+public static void main(String[] args)
+{
+    int n = 9;
+
+    Arrays.fill(dp, -1);
+
+    System.out.print(fib(n));
+}
+}

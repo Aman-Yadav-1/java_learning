3a57ca0df13ca3482934a3aa8a4e84fb036dde9d
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-03 17:51:23 +0530

    m2

---
+++ gfg_fork/fibonacci_numbers/method2.java
@@ -0,0 +1,22 @@
+package gfg_fork.fibonacci_numbers;
+// this method will use Dynamic Programming to get fibonacci number
+// in this method we are reducing the repeated work done in method1
+public class method2 {
+    static int fib(int n){
+        // declaring array
+        int f[] = new int[n+2];
+        int i;
+
+        f[0]=0;
+        f[1]=1;
+
+        for(i=2;i<=n;i++){
+            f[i]=f[i-1]+f[i-2];
+        }
+        return f[n];
+    }
+    public static void main(String [] args){
+        int n=9;
+        System.out.println(fib(n));
+    }
+}

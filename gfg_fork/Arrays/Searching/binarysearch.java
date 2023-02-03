fc0c00f97b38b21ec8f53061516352e312cacc18
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 02:32:01 +0530

    bsa

--- gfg_fork/Arrays/Searching/binarysearch.java
+++ gfg_fork/Arrays/Searching/binarysearch.java
@@ -3,26 +3,28 @@ import java.util.*;
 
 public class binarysearch {
 
-    static int one(int arr[], int n){
+    static int one(int arr[],int n){
         int low = 0;
         int high = n - 2;
         int mid;
 
-        while(low<=high){
-            mid=(low+high)/2;
+        while(low <= high){
+            mid = (low+high)/2;
+            
             if(arr[mid]==arr[mid^1]){
-                low=mid+1;
+                low = mid+1;
             }
             else{
-                high=mid-1;
+                high = mid-1;
             }
         }
         return arr[low];
     }
-    public static void main(String[] args){
-        int arr[] = {2,3,5,3,4,2,5,4,7};
-        int size = 9;
+    public static void main(String [] args){
+        int arr[] = {7,2,3,4,2,3,4,5,5};
+        int size = arr.length;
         Arrays.sort(arr);
+
         System.out.println(one(arr,size));
     }
 }

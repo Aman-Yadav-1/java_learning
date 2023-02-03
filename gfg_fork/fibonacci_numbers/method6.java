0a43dafd3e9c6f7ae22ab509686c55568549b63c
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 01:32:43 +0530

    m6m7

---
+++ gfg_fork/fibonacci_numbers/method6.java
@@ -0,0 +1,43 @@
+package gfg_fork.fibonacci_numbers;
+//alternate of method5
+public class method6 {
+    static int MAX = 1000;
+    static int f[];
+    
+    // Returns n'th fibonacci number using 
+    // table f[]
+    public static int fib(int n)
+    {
+        // Base cases
+        if (n == 0)
+            return 0;
+            
+        if (n == 1 || n == 2)
+            return (f[n] = 1);
+     
+        // If fib(n) is already computed
+        if (f[n] != 0)
+            return f[n];
+     
+        int k = (n & 1) == 1? (n + 1) / 2 
+                            : n / 2;
+     
+        // Applying above formula [Note value
+        // n&1 is 1 if n is odd, else 0.
+        f[n] = (n & 1) == 1? (fib(k) * fib(k) + 
+                        fib(k - 1) * fib(k - 1))
+                       : (2 * fib(k - 1) + fib(k)) 
+                       * fib(k);
+     
+        return f[n];
+    }
+    
+    /* Driver program to test above function */
+    public static void main(String[] args) 
+    {
+        int n = 9;
+        f= new int[MAX];
+        System.out.println(fib(n));
+    }
+}
+//time complexity : O(Logn)

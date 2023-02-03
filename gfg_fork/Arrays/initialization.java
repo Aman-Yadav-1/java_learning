17cce5534407b1cf88466c9830d4fa2967ad1abc
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-03 16:32:37 +0530

    first

---
+++ gfg_fork/Arrays/initialization.java
@@ -0,0 +1,11 @@
+package gfg_fork.Arrays;
+
+public class initialization {
+        public static void main(String args[]) {
+           int arr[] = {10, 20, 30, 40, 50};
+           for(int i=0; i < arr.length; i++)
+           {
+                 System.out.print(" " + arr[i]);              
+           }
+        }
+}

314e1042ac75770b8dafc9ec60535efe51c9c919
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 01:34:03 +0530

    m7

--- gfg_fork/fibonacci_numbers/method7.java
+++ gfg_fork/fibonacci_numbers/method7.java
@@ -3,5 +3,12 @@ package gfg_fork.fibonacci_numbers;
 //in this we will use Fn = {[(√5 + 1)/2] ^ n} / √5 
 
 public class method7 {
-    
+
+    static int fib(int n){
+        
+    }
+    public static void main(String [] args){
+        int n = 9;
+        System.out.println(fib(n));
+    }
 }

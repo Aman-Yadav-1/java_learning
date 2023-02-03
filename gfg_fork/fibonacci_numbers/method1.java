197b7d4fef6762955ac3f255fb40eaa44ba8916b
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-03 17:52:51 +0530

    m1

--- gfg_fork/fibonacci_numbers/method1.java
+++ gfg_fork/fibonacci_numbers/method1.java
@@ -3,6 +3,8 @@ package gfg_fork.fibonacci_numbers;
 //fibonacci = precedor + successor= new successor
 //finomacci numbers : 0 1 1 2 3 5 8 13 21 34 55
 //Index :             0 1 2 3 4 5 6 7  8  9  10 
+
+
 public class method1 {
     static int fib(int n){
         if(n<=1)
---
+++ gfg_fork/fibonacci_numbers/method3.java
@@ -0,0 +1,6 @@
+package gfg_fork.fibonacci_numbers;
+
+public class method3 {
+    public static void main(String [] args){
+    }
+}

03ca898d0b3de28e838b7d5d9bd6a9d4654a13c4
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 01:46:02 +0530

    m7

--- gfg_fork/fibonacci_numbers/method7.java
+++ gfg_fork/fibonacci_numbers/method7.java
@@ -3,6 +3,12 @@ package gfg_fork.fibonacci_numbers;
 //in this we will use Fn = {[(√5 + 1)/2] ^ n} / √5 
 
 public class method7 {
-
-    
+    static int fib(int n){
+        double phi = (1+ Math.sqrt(5))/2;
+        return (int) Math.round(Math.pow(phi,n)/Math.sqrt(5));
+    }
+    public static void main(String [] args){
+        int n = 9;
+        System.out.println(fib(n));
+    }
 }

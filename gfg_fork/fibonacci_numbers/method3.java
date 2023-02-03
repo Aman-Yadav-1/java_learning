7beb6994e4a977b57c26164916e639979432fd86
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-03 17:59:56 +0530

    m2

--- gfg_fork/fibonacci_numbers/method3.java
+++ gfg_fork/fibonacci_numbers/method3.java
@@ -1,6 +1,21 @@
 package gfg_fork.fibonacci_numbers;
-
+//optimized version of method2
 public class method3 {
-    public static void main(String [] args){
+    static int fib(int n){
+        int a=0,b=1,c;
+        if(n==0){
+            return a;
+        }
+
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+    public static void main(String[] args){
+        int n=9;
+        System.out.println(fib(n));
     }
 }

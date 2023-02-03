1beaf84f469e5d0777adb59b6ed1486d7b399538
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-03 17:28:08 +0530

    3d

---
+++ gfg_fork/Arrays/threeD_array.java
@@ -0,0 +1,15 @@
+package gfg_fork.Arrays;
+
+public class threeD_array {
+    public static void main(String [] args){
+    int[][][]arr={{{1,2},{3,4}},{{5,6},{7,8}}};
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            for(int k=0;k<2;k++){
+                System.out.println("arr["+i+"]"+"arr["+j+"]"+"arr["+k+"]"+"="+arr[i][j][k]);
+            }
+        }
+    }
+}
+}

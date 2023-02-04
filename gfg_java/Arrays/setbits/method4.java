6166b03e8220982e4607db15d7a2c3bf0166d53a
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 14:42:01 +0530

    m4

--- gfg_java/Arrays/setbits/method4.java
+++ gfg_java/Arrays/setbits/method4.java
@@ -2,5 +2,39 @@
 package gfg_java.Arrays.setbits;
 
 public class method4 {
-    
+    // Lookup table
+    static int[] BitsSetTable256 = new int[256];
+
+    // Function to initialise the lookup table
+    public static void initialize()
+    {
+
+        // To initially generate the
+        // table algorithmically
+        BitsSetTable256[0] = 0;
+        for (int i = 0; i < 256; i++) {
+            BitsSetTable256[i] = (i & 1) + BitsSetTable256[i / 2];
+        }
+    }
+
+    // Function to return the count
+    // of set bits in n
+    public static int countSetBits(int n)
+    {
+        return (BitsSetTable256[n & 0xff]
+                + BitsSetTable256[(n >> 8) & 0xff]
+                + BitsSetTable256[(n >> 16) & 0xff]
+                + BitsSetTable256[n >> 24]);
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+
+        // Initialise the lookup table
+        initialize();
+        int n = 9;
+        System.out.print(countSetBits(n));
+    }
+
 }

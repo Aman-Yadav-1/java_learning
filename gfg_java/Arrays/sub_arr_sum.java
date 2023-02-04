58d79fce7ee2fd071781b862c391c22fc49cbbe4
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 14:54:09 +0530

    subarraysum

---
+++ gfg_java/Arrays/sub_arr_sum.java
@@ -0,0 +1,10 @@
+// Subset array sum by generating all the subsets
+package gfg_java.Arrays;
+
+import java.util.*;
+
+public class sub_arr_sum {
+    public static void main(String [] args){
+        
+    }
+}

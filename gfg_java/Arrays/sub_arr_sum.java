cf43c8bedf53de318ffcc53bc3b4b86269e309ce
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-04 15:00:38 +0530

    subarray

--- gfg_java/Arrays/sub_arr_sum.java
+++ gfg_java/Arrays/sub_arr_sum.java
@@ -1,10 +1,28 @@
 // Subset array sum by generating all the subsets
 package gfg_java.Arrays;
 
-import java.util.*;
 
 public class sub_arr_sum {
+    static void sub(int arr[], int length,int s){
+    for(int i = 0; i<=Math.pow(2,length); i++){
+            int sum = 0;
+            for(int j = 0;j < length;j++){
+                if(((i >> j) & 1) % 2 == 1){
+                    sum += arr[j];
+                }
+            }
+            if(sum == s){
+                System.out.println("YES");
+                return;
+            }
+        }
+        System.out.println("NO");
+    }
+    
     public static void main(String [] args){
-        
+        int sum = 5;
+        int arr[] = {-1,2,4,121};
+        int length = arr.length;
+        sub(arr,length,sum);
     }
 }

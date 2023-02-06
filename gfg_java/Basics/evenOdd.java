4f0e31741423ef692d1179685d57439b86b78777
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-06 08:55:27 +0530

    evenOdd

---
+++ gfg_java/Basics/evenOdd.java
@@ -0,0 +1,19 @@
+// Given two integers A and B. 
+// Your task is to print the even number first and odd number next in individual lines.
+package gfg_java.Basics;
+
+public class evenOdd {
+    //pseudo code
+    static void evenOdd (int a,int b) 
+	 {
+		//Your code here
+	     if(a%2==0){
+	         System.out.println(a);
+	         System.out.println(b);
+	     }
+	     else{
+	         System.out.println(b);
+	         System.out.println(a);
+	     }
+	 }
+}

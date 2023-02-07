d9471c3c0073a7e9c5f9dc433c2528a6b042c637
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 03:46:39 +0530

    p9

--- DS/Patterns/ApnaColllege/Pattern9.java
+++ DS/Patterns/ApnaColllege/Pattern9.java
@@ -6,11 +6,15 @@ public class Pattern9 {
     public static void main(String [] args){
     Scanner scan = new Scanner(System.in);
     int n = scan.nextInt();
-    int val=1;
 
     for(int i = 1;i<=n;i++){
         for(int j = 1;j<=i;j++){
-            System.out.print(val);
+            if((i+j)%2==0){
+                System.out.print("1 ");
+            }
+            else{
+                System.out.print("0 ");
+            }
         }
         System.out.println();
     }

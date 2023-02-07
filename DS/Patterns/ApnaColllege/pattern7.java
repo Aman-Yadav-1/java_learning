ff39169fa54a272735045454f47680101d12725f
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 03:29:32 +0530

    p7

--- DS/Patterns/ApnaColllege/pattern7.java
+++ DS/Patterns/ApnaColllege/pattern7.java
@@ -6,8 +6,8 @@ public class pattern7 {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
                 System.out.print(j);
             }
             System.out.println();

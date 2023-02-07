d9ba78b913e1a323089c48cdad1cb043a9daf83e
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 02:59:24 +0530

    p2

--- DS/Patterns/ApnaColllege/Pattern2.java
+++ DS/Patterns/ApnaColllege/Pattern2.java
@@ -1,5 +1,15 @@
 package ApnaColllege;
-
+import java.util.*;
 public class Pattern2 {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                
+            }
+        }
+        scan.close();
+    }
 }

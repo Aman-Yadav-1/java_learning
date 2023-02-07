a3d6c1853955eed2fdcdf81622e9d0753fee77b7
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 03:20:16 +0530

    p5

--- DS/Patterns/ApnaColllege/Pattern5.java
+++ DS/Patterns/ApnaColllege/Pattern5.java
@@ -6,16 +6,16 @@ public class Pattern5 {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        int space=n-2;
+        int space=n-1;
         int star=1;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=space;j++){
                 System.out.print(" ");
             }
             for(int j=1;j<=star;j++){
                 System.out.print("*");
             }
-            if(i<n){
+            if(i<=n){
                 space--;
                 star++;
             }

481a00dbc3bf3b818f0599d0935dddb045c8be87
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 16:35:34 +0530

    p1

--- ApnaColllege/advancedpatterns/Pattern1.java
+++ ApnaColllege/advancedpatterns/Pattern1.java
@@ -1,3 +1,34 @@
+//Butterfly pattern
+import java.util.*;
+
 public class Pattern1 {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int space = n+2;
+        int star = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+
+            if(i<=n/2){
+                star++;
+                space-=2;
+            }
+            else{
+                star--;
+                space+=2;
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
 }

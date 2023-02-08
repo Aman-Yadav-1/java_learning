046694e2656b665d12aa690f199aa13d4779e5ce
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 16:59:54 +0530

    p2

---
+++ ApnaColllege/advancedpatterns/pattern2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class pattern2 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int space=n-1;
+        int star=n;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            space--;
+            System.out.println();
+        }
+        scan.close();
+    }
+}

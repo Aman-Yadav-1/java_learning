1470daa4232b5db066e16d063405f6d2f621d5b6
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-08 17:25:22 +0530

    5

---
+++ ApnaColllege/advancedpatterns/pattern4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class pattern4 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=2;j--){
+                System.out.print(j);
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}

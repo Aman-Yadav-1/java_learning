3af784373190250368b51fba5a21890bd233812e
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-09 17:02:53 +0530

    p1

---
+++ striver_java/Patterns/Pattern6.java
@@ -0,0 +1,17 @@
+package striver_java.Patterns;
+import java.util.*;
+
+public class Pattern6 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
---
+++ striver_java/Patterns/Pattern7.java
@@ -0,0 +1,16 @@
+package striver_java.Patterns;
+
+public class Pattern7 {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}

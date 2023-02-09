2faa070c01d403eaa8fd6d6597a5450470486583
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-09 17:06:18 +0530

    p2

--- striver_java/Patterns/Pattern4.java
+++ striver_java/Patterns/Pattern4.java
@@ -1,14 +1,14 @@
 package striver_java.Patterns;
 import java.util.*;
 
-public class Pattern6 {
+public class Pattern4 {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        for(int i=n;i>=1;i--){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print("* ");
+                System.out.print(i);
             }
             System.out.println();
         }
--- striver_java/Patterns/Pattern5.java
+++ striver_java/Patterns/Pattern5.java
@@ -6,9 +6,9 @@ public class Pattern5 {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        for(int i=1;i<=n;i++){
+        for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
-                System.out.print(i);
+                System.out.print("* ");
             }
             System.out.println();
         }

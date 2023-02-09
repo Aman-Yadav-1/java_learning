54aad51f0da940d4c44a29dd06ae5bbbd1935021
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-09 17:07:08 +0530

    p6

--- striver_java/Patterns/Pattern6.java
+++ striver_java/Patterns/Pattern6.java
@@ -8,7 +8,7 @@ public class Pattern6 {
 
         for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
-                System.out.print("* ");
+                System.out.print(j);
             }
             System.out.println();
         }

2371c762cf7f2c8c92c99a83ba1585520016cda2
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 03:48:41 +0530

    int

--- ApnaCollege/Array/intro.java
+++ ApnaCollege/Array/intro.java
@@ -4,7 +4,6 @@ public class intro {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
         int size=scan.nextInt();
-
         int [] arr = new int[size];
 
         for(int i=0;i<size;i++){

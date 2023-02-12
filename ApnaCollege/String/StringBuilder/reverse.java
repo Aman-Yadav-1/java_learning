c715742e20679761b13107a890dc9003529a5f46
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-13 00:15:08 +0530

    reverse a string

--- ApnaCollege/String/StringBuilder/reverse.java
+++ ApnaCollege/String/StringBuilder/reverse.java
@@ -5,7 +5,19 @@ public class reverse {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
         StringBuilder sb = new StringBuilder(scan.next());
-        System.out.println(sb.reverse());
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front = i;
+            int back = sb.length() - 1 -i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front,backChar);
+            sb.setCharAt(back,frontChar);
+        }
+
+        System.out.println(sb);
         scan.close();
     }
 }

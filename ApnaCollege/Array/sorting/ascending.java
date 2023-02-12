c2ddd6f12ab684a99bcc341eac1629af5569d9d2
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 13:39:52 +0530

    asc

---
+++ ApnaCollege/Array/sorting/ascending.java
@@ -0,0 +1,31 @@
+package sorting;
+import java.util.*;
+
+public class ascending {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+
+        int arr[] = new int[size];
+
+        for(int i=0;i<size;i++){
+            arr[i] = scan.nextInt();
+        }
+
+        boolean isAscending = true;
+
+        for(int i = 0;i< arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                isAscending = false;
+            }
+        }
+
+        if(isAscending){
+            System.out.println("The array is in ascending order");
+        }
+        else{
+            System.out.println("array is in discending order");
+        }
+        scan.close();
+    }
+}

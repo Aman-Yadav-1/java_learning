aba7570dd2c080ec0c5fc75d5afa5712719f4264
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 13:16:53 +0530

    mm

--- ApnaCollege/Array/minmax.java
+++ ApnaCollege/Array/minmax.java
@@ -1,1 +1,13 @@
 import java.util.*;
+
+public class minmax{
+    public static void main(String [] args){
+        Scanner scan  = new Scanner(System.in);
+        int size = scan.nextInt();
+
+        int arr[] = new int [size];
+
+        scan.close();
+
+    }
+}

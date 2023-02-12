2707211d37bc3beb269c690a0f093e4b4868c2ac
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 14:55:16 +0530

    transpose of a matrix

--- ApnaCollege/Array/two_d_array/transpose.java
+++ ApnaCollege/Array/two_d_array/transpose.java
@@ -3,23 +3,24 @@ import java.util.*;
 public class transpose {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
-
-        int row = scan.nextInt();
+        int row  = scan.nextInt();
         int col = scan.nextInt();
 
         int arr[][] = new int[row][col];
 
         for(int i = 0;i<row;i++){
-            for(int j = 0;j<col;j++){
-                arr[i][j] = scan.nextInt();
+            for(int j=0;j<col;j++){
+                arr[i][j]=scan.nextInt();
             }
         }
-        System.out.println("Transpose of this matrix is : ");
+
+        System.out.println("The transpose of this matrix is : ");
 
         for(int j = 0;j < col;j++){
             for(int i = 0;i < row;i++){
                 System.out.print(arr[i][j]+" ");
             }
+
             System.out.println();
         }
         scan.close();

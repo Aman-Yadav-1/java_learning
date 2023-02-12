4c9071c8589cb4df19736f360c2c2ce05141f174
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 13:28:12 +0530

    mm

--- ApnaCollege/Array/minmax.java
+++ ApnaCollege/Array/minmax.java
@@ -14,7 +14,7 @@ public class minmax{
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
-        for(int i=0;i<size;i++){
+        for(int i=0;i<arr.length;i++){
             if(arr[i] < min){
                 min = arr[i];
             }

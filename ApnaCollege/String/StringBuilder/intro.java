220c754cf4198dba97370fb2b57de01abc2c8029
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 23:32:51 +0530

    intro

--- ApnaCollege/String/StringBuilder/intro.java
+++ ApnaCollege/String/StringBuilder/intro.java
@@ -2,7 +2,19 @@ package ApnaCollege.String.StringBuilder;
 
 public class intro {
     public static void main(String [] args){
-        StringBuilder sb = new StringBuilder("StringBuilt");
+        StringBuilder sb = new StringBuilder("tuny");
+        System.out.println(sb);
+        
+        sb.setCharAt(0,'s'); //replace character at 0 index with given new char
+        System.out.println(sb);
+
+        sb.insert(2,'n'); // inset new value at index 2
+        System.out.println(sb);
+
+        sb.delete(2,3); // deletes from index:2 to 3
+        System.out.println(sb);
+
+        sb.append("deol"); //adds at last
         System.out.println(sb);
     }
 }

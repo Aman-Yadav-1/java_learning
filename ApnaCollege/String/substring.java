487e4d909db18ae3bf3246534d3f71d8de70b009
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 22:11:20 +0530

    substring

---
+++ ApnaCollege/String/substring.java
@@ -0,0 +1,10 @@
+package ApnaCollege.String;
+
+public class substring {
+    public static void main(String [] args){
+        String sentence = "My Name Is AmanYadav";
+        String name = sentence.substring(11);
+
+        System.out.println(name);
+    }
+}

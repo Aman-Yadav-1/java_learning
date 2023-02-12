b70ab76ef5e000021651c8e7011afb77ce2d19c9
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 14:24:00 +0530

    initializing 2d array

--- ApnaCollege/Array/two_d_array/initialization.java
+++ ApnaCollege/Array/two_d_array/initialization.java
@@ -1,5 +1,21 @@
 package two_d_array;
 import java.util.*;
+
 public class initialization {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        int row = scan.nextInt();
+        int col = scan.nextInt();
+
+        int arr [][] = new int[row][col];
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr [i][j]=scan.nextInt();
+            }
+        }
+
+        scan.close();
+    }
 }

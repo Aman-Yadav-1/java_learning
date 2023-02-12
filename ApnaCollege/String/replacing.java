77a9890b4a945ccf42002dd16ccb8fb2536d6016
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 22:48:22 +0530

    replacing an element from string

---
+++ ApnaCollege/String/replacing.java
@@ -0,0 +1,22 @@
+package ApnaCollege.String;
+import java.util.*;
+
+public class replacing {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        String result = "";
+
+        for(int i = 0;i<str.length();i++){
+            if(str.charAt(i)=='e'){
+                result += 'i';
+            }
+            else{
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+        scan.close();
+    }
+}

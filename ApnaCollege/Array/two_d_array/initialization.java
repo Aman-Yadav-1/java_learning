04ec0b2c0d5f5adcb9969af8823afb4fdf6e054c
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 14:29:39 +0530

    init

--- ApnaCollege/Array/two_d_array/initialization.java
+++ ApnaCollege/Array/two_d_array/initialization.java
@@ -16,6 +16,12 @@ public class initialization {
             }
         }
 
+        for(int i = 0;i<row;i++){
+            for(int j=0;j<col;j++){
+            System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
         scan.close();
     }
 }

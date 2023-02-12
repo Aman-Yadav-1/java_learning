0b4fd49d73d956369359fff4f8473b87e3c3be0c
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-12 22:51:17 +0530

    replacing character from string

--- ApnaCollege/String/replacing.java
+++ ApnaCollege/String/replacing.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class replacing {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
-
         String str = scan.next();
         String result = "";
 

3bf3a8b13b640d31f3132e05f9db438ab3867ae5
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-13 17:04:23 +0530

    updateBit

--- ApnaCollege/bitmani/updateBit.java
+++ ApnaCollege/bitmani/updateBit.java
@@ -1,5 +1,23 @@
 package ApnaCollege.bitmani;
+import java.util.*;
 
 public class updateBit {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = 5;
+        int bm = 1<<1;
+
+        int operation = scan.nextInt();
+        if(operation==1){
+            int newNum=bm|n;
+            System.out.println(newNum);
+        }
+        else{
+            int newNum = (~bm) & n;
+            System.out.println(newNum);
+        }
+
+        scan.close();
+    }
 }

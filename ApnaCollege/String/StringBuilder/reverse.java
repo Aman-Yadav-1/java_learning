aef5ac53a9bc8e1651cd817bd4defb96f3c10c32
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-13 15:07:54 +0530

    reverse a string using stringBuilder

--- ApnaCollege/String/StringBuilder/reverse.java
+++ ApnaCollege/String/StringBuilder/reverse.java
@@ -9,7 +9,6 @@ public class reverse {
         for(int i=0;i<sb.length()/2;i++){
             int front = i;
             int back = sb.length() - 1 - i;
-
             char frontChar = sb.charAt(front);
             char backChar = sb.charAt(back);
 

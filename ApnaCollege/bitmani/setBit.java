8e9f39da438d7f1f5939b226530027b9784b3c40
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-13 16:34:20 +0530

    setBit

--- ApnaCollege/bitmani/setBit.java
+++ ApnaCollege/bitmani/setBit.java
@@ -1,5 +1,16 @@
 package ApnaCollege.bitmani;
+import java.util.*;
 
 public class setBit {
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int i = 1;
+        int bm = 1<<i;
+
+        int newNum = bm | n;
+        System.out.println(newNum);
+        scan.close();
+    }
 }

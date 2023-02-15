0c4423322a7d66222707f3d5a4b611472a64bb6a
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-15 16:05:15 +0530

    selectionSort

--- ApnaCollege/Sorting/selectionSort.java
+++ ApnaCollege/Sorting/selectionSort.java
@@ -1,7 +1,9 @@
 package ApnaCollege.Sorting;
 // selectionSort algorithm
-// compare two elements in array and push small element in lower index
+// compare two elements in array and smallest of two get swapped down 
 
 public class selectionSort {
-    
+    public static void main(String [] args){
+
+    }
 }

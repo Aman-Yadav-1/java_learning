889de113830760e02cdf8ebfd52eab3e91b696af
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-15 16:34:47 +0530

    Insertion Sorting Algorithm

--- ApnaCollege/Sorting/insertionSort.java
+++ ApnaCollege/Sorting/insertionSort.java
@@ -1,5 +1,6 @@
 package ApnaCollege.Sorting;
 // Insertion Sorting Algorithm
+
 // In this we divide array in two parts, sorted and unsorted part
 // we assume one element of at leftmost side to be in sorted array and rest in unsorted
 // we compare each and everyelement from unsorted with sorted one, and place unsorted as per its ability

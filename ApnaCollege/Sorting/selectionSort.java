8d7e4e6632c8e30428b336938fe9d5fe728e5105
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-15 16:20:03 +0530

    Selection Sorting Algorithm

--- ApnaCollege/Sorting/selectionSort.java
+++ ApnaCollege/Sorting/selectionSort.java
@@ -3,22 +3,23 @@ package ApnaCollege.Sorting;
 // compare two elements in array and smallest of two get swapped down 
 
 public class selectionSort {
-    public static void print(int [] arr){
-        for(int i = 0; i < arr.length;i++){
+    public static void print(int arr[]){
+        for(int i = 0;i < arr.length;i++){
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
     public static void main(String [] args){
-        int arr[] = {8,7,4,2,3,1};
+        int arr[] = {7,8,3,4,1,2};
 
         for(int i = 0;i < arr.length - 1;i++){
             int smallest = i;
-            for(int j = i + 1;j < arr.length; j++){
+            for(int j = i + 1;j < arr.length;j++){
                 if(arr[smallest] > arr[j]){
                     smallest = j;
                 }
             }
+
             int temp = arr[smallest];
             arr[smallest] = arr[i];
             arr[i] = temp;

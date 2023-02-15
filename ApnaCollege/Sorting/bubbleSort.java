3dc51996d0802b546214da9d2e6720352aaed83e
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-15 15:51:08 +0530

    bubble sort

--- ApnaCollege/Sorting/bubbleSort.java
+++ ApnaCollege/Sorting/bubbleSort.java
@@ -1,16 +1,16 @@
 package ApnaCollege.Sorting;
 
 public class bubbleSort{
-    public static void print(int [] arr){
+    public static void print(int arr[]){
         for(int i = 0;i < arr.length;i++){
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
     public static void main(String [] args){
-        int arr[] = {7,8,2,3,1};
+        int arr[] = {8,3,7,2,1};
 
-        for(int i = 0;i < arr.length - 1;i++){
+        for(int i = 0; i < arr.length - 1;i++){
             for(int j = 0;j < arr.length - i - 1;j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];

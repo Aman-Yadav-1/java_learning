0f059fc99e8d02ff0e0c459df4bb1febff2548ca
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-15 16:26:25 +0530

    insertion Sort

--- ApnaCollege/Sorting/insertionSort.java
+++ ApnaCollege/Sorting/insertionSort.java
@@ -1,10 +1,15 @@
 package ApnaCollege.Sorting;
-
+//
 public class insertionSort {
     public static void print(int arr[]){
         for(int i = 0;i < arr.length;i++){
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+    }
+
+    public static void main(String [] args){
+        int arr[] = {7,8,4,2,3,1};
+
     }
 }

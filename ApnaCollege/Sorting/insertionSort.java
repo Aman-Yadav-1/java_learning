d7cce4183eb81eb343e0e2184069b122f2262623
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-15 16:37:58 +0530

    Insertion Sorting Algorithm

--- ApnaCollege/Sorting/insertionSort.java
+++ ApnaCollege/Sorting/insertionSort.java
@@ -17,14 +17,12 @@ public class insertionSort {
         int arr[] = {7,8,4,2,3,1};
 
         for(int i = 1; i < arr.length;i++){
-            int current = arr[i];
+            int current  = arr[i];
             int j = i - 1;
-
             while(j >= 0 && current < arr[j]){
                 arr[j+1] = arr[j];
                 j--;
             }
-
             arr[j+1] = current;
         }
 

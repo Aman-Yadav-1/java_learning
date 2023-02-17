a45e16522b6725941772f1d7b0cc6f287bd3e91e
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-17 18:24:47 +0530

    sum of n natural numbers

--- ApnaCollege/recursion/sumofn.java
+++ ApnaCollege/recursion/sumofn.java
@@ -2,5 +2,16 @@ package ApnaCollege.recursion;
 // Sum of n natural number using recursion
 
 public class sumofn {
-    
+    public static void sumNum(int i, int n, int sum){
+        if(i==n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        sumNum(i+1,n,sum);
+    }
+    public static void main(String [] args){
+        sumNum(1,5, 0);
+    }
 }

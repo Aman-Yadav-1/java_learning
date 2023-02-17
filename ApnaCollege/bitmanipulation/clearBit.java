88fe3621040297bed96f9d72609894007ee3f310
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-17 16:50:36 +0530

    clearBit

---
+++ ApnaCollege/bitmanipulation/clearBit.java
@@ -0,0 +1,13 @@
+package ApnaCollege.bitmani;
+
+public class clearBit {
+    public static void main(String [] args){
+        int n = 5;
+        int i = 2;
+        int bm = 1<<i;
+
+        bm=~bm;
+        int newNum = bm & n;
+        System.out.println(newNum);
+    }
+}

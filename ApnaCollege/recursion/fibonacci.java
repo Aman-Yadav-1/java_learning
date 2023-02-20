4f01667a2e97c0ed4e1092fdf72bdc7181820f8f
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-20 17:16:05 +0530

    fibonacci

--- ApnaCollege/recursion/fibonacci.java
+++ ApnaCollege/recursion/fibonacci.java
@@ -1,12 +1,13 @@
 package ApnaCollege.recursion;
 
 public class fibonacci {
-    public static int fib(int n){
+    public static int fib(int a,int b,int n){
         if(n==0){
             return 0;
         }
     }
     public static void main(String [] args){
-        int a = ,b = ;
+        int a = 0;
+        int b = 1;
     }
 }

98cf54fb0a3a1aa9a910122fea8b7f45f104c60d
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-20 17:11:08 +0530

    factorial using recursion

--- ApnaCollege/recursion/factorial.java
+++ ApnaCollege/recursion/factorial.java
@@ -1,2 +1,16 @@
 package ApnaCollege.recursion;
-public class factorial{}
+public class factorial{
+    public static int calcfactorial(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        int fact_nm1 = calcfactorial(n-1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+    }
+    public static void main(String [] args){
+        int n = 5;
+        int ans = calcfactorial(n);
+        System.out.println(ans);
+    }
+}

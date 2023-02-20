98b7401ca12f14f21a860513d63818c1547d0255
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-20 17:18:49 +0530

    fibonacci using recursion

--- ApnaCollege/recursion/fibonacci.java
+++ ApnaCollege/recursion/fibonacci.java
@@ -2,11 +2,20 @@ package ApnaCollege.recursion;
 
 public class fibonacci {
     public static void fib(int a,int b,int n){
+        if(n==0){
+            return;
+        }
         int c = a + b;
         System.out.println(c);
+        fib(b,c,n-1);
     }
     public static void main(String [] args){
         int a = 0;
         int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+
+        int n = 7;
+        fib(a, b, n-2);
     }
 }

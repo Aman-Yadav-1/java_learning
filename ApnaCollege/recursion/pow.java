67782e4c3bb396eb2b1aad2bf3e2b0080234366d
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-23 01:50:48 +0530

    power

--- ApnaCollege/recursion/pow.java
+++ ApnaCollege/recursion/pow.java
@@ -1,22 +1,5 @@
 package ApnaCollege.recursion;
 
 public class pow {
-    public static int calcPower(int x,int n){
-        if(n == 0){
-            return 1;
-        }
-        if(x == 0){
-            return 0;
-        }
-
-        int xPow_nm1 = calcPower(x, n-1);
-        int xPow_n = x * xPow_nm1;
-        return xPow_n;
-    }
-    public static void main(String [] args){
-        int x = 2;
-        int n = 3;
-
-        System.out.println(calcPower(x, n));
-    }
+    
 }

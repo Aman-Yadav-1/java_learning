b52bf839a3799abf25ec7a74e994ab6774a34c63
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-23 02:45:10 +0530

    toh

--- ApnaCollege/recursion/tower_of_hanoi.java
+++ ApnaCollege/recursion/tower_of_hanoi.java
@@ -1,6 +1,17 @@
 package ApnaCollege.recursion;
 
 public class tower_of_hanoi {
+    public static void toh(int n, String src, String helper, String dest){
+        if(n == 1){
+            System.out.println("Transfer Disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+        toh(n-1, src, dest, helper);
+        System.out.println("Transfer Disk " + n + " from " + src + " to " + dest);
+        toh(n-1, helper, src, dest);
+    }
     public static void main(String [] args){
+        int n = 3;
+        toh(n, "S", "H", "D");
     }
 }

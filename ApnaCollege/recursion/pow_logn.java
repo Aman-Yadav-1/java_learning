634328884c4e4bc61afe54205195aaa0335c832e
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-23 02:15:09 +0530

    power at logn stack height

--- ApnaCollege/recursion/pow_logn.java
+++ ApnaCollege/recursion/pow_logn.java
@@ -2,5 +2,28 @@ package ApnaCollege.recursion;
 // optimized method of power question
 
 public class pow_logn {
-    
+    public static int calcPow(int x,int n){
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+
+        if(n%2 == 0){
+            return calcPow(x, n/2) * calcPow(x, n/2);
+        }
+        else{
+            return calcPow(x, n/2) * calcPow(x, n/2) * x;
+        }
+
+    }
+    public static void main(String [] args){
+        int x = 2;
+        int n = 5;
+
+        int res = calcPow(x, n);
+
+        System.out.println(res);
+    }
 }

4873c3bbddec46943543274a6a38d9edc5e1e808
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-23 15:22:15 +0530

    reverse a string

---
+++ ApnaCollege/recursion/reverseString.java
@@ -0,0 +1,16 @@
+package ApnaCollege.recursion;
+
+public class reverseString {
+    public static void revStr(String str, int idx){
+        if(idx==0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        revStr(str, idx-1);
+    }
+    public static void main(String [] args){
+        String str = "abcd";
+        revStr(str, str.length()-1);
+    }
+}

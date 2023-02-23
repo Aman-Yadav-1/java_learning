f73081fef23c94443032b1b752018f045d20288b
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-23 15:53:49 +0530

    first and last occurence in string

--- ApnaCollege/recursion/first_last.java
+++ ApnaCollege/recursion/first_last.java
@@ -4,7 +4,7 @@ public class first_last {
     public static int first = -1;
     public static int last = -1;
 
-    public static void fl(String str,int idx, char elem){
+    public static void findOcc(String str, int idx, char elem){
         if(idx == str.length()){
             System.out.println(first);
             System.out.println(last);
@@ -19,10 +19,10 @@ public class first_last {
                 last = idx;
             }
         }
-        fl(str, idx+1, elem);
+        findOcc(str, idx+1, elem);
     }
     public static void main(String [] args){
-        String str = "acbaacdaefaah";
-        fl(str, 0, 'c');
+        String str = "abaacdaefaah";
+        findOcc(str,0,'a');
     }
 }

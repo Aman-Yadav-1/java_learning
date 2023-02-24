ceb3117d6d844ab938f40e0c76e5d67cf383a3f8
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-24 20:16:05 +0530

    n queens

--- ApnaCollege/backtracking/n_queens.java
+++ ApnaCollege/backtracking/n_queens.java
@@ -1,5 +1,9 @@
 package ApnaCollege.backtracking;
-
+import java.util.*;
+public void helper(char [][]board, L)
 public class n_queens {
-    
+    public List<List<String>> solveNQueens(int n){
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new board[n][n];
+    }
 }

c65dd932f0219986f08d2f0382ecb459ce3216ab
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-24 16:20:55 +0530

    backtracking

--- ApnaCollege/backtracking/bt.java
+++ ApnaCollege/backtracking/bt.java
@@ -1,7 +1,19 @@
 package ApnaCollege.backtracking;
 
 public class bt{
+    public static void printPermutation(String str, String permu, int idx){
+        if(str.length() == 0){
+            System.out.println(permu);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0,i)+str.substring(i+1);
+            printPermutation(newStr, permu + currChar, idx+1);
+        }
+    }
     public static void main(String [] args){
         String str = "ABC";
+        printPermutation(str, "", 0);
     }
 }

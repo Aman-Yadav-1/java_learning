334d1f614fa94c035947b09e03a80b85fbfdc0bf
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-25 15:24:38 +0530

    quick sort technique

--- ApnaCollege/Sorting/quickSort.java
+++ ApnaCollege/Sorting/quickSort.java
@@ -11,29 +11,31 @@ public class quickSort {
                 i++;
                 int temp = arr[i];
                 arr[i] = arr[j];
-                arr[j] = temp;
+                arr[j] = temp; 
             }
         }
+
         i++;
         int temp = arr[i];
         arr[i] = pivot;
         arr[high] = temp;
+
         return i;
     }
     public static void quicksort(int arr[], int low, int high){
 
         if(low < high){
-            int pivdx = partition(arr,low,high);
+            int pivdx = partition(arr, low, high);
 
             quicksort(arr, low, pivdx-1);
-            quicksort(arr, pivdx+1, high);
+            quicksort(arr, pivdx + 1, high);
         }
     }
     public static void main(String [] args){
-        int arr[] = {6,3,4,7,2,8,5,9};
+        int arr[] = {6,3,4,5,8,9,7};
         int n = arr.length;
 
-        quicksort(arr, 0, n-1);
+        quicksort(arr,0,n-1);
 
         for(int i = 0;i < n;i++){
             System.out.print(arr[i] + " ");

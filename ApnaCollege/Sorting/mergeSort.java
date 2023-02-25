6fd13b798b30cc9d9a94d58585b4ce760cdf439b
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-02-25 13:05:47 +0530

    mergeSort technique

--- ApnaCollege/Sorting/mergeSort.java
+++ ApnaCollege/Sorting/mergeSort.java
@@ -1,4 +1,8 @@
 package ApnaCollege.Sorting;
 
-public class mergeSort {   
+public class mergeSort { 
+    public static void main(String [] args){
+        int arr[] = {6,3,9,5,2,8};
+        System.out.println(arr);
+    }  
 }

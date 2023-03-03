4b295c3d6d93d52052faf337712c7e611c13aaa0
Aman-yadav-ay <amxn.yadav@gmail.com>
2023-03-03 14:41:52 +0530

    linked list

--- ApnaCollege/linkedList/LL.java
+++ ApnaCollege/linkedList/LL.java
@@ -1,5 +1,14 @@
 package ApnaCollege.linkedList;
 
 class LL {
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static void main(String [] args){}
 }
